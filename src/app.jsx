91c07a41edae89ffec161c7bc299801c427c65c0
Emma <emma.slinkov@gmail.com>
2025-11-04 20:13:50 -0700

    App component login React

--- src/app.jsx
+++ src/app.jsx
@@ -1,6 +1,7 @@
 // This is the top level App component
 
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
 
 // import Boostrtap so the styling shows up
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -9,21 +10,54 @@ import 'bootstrap-icons/font/bootstrap-icons.css';
 // import the top level CSS from our main CSS file we named app.css
 import './app.css';
 
-import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+// page components
 import { Login } from './login/login';
 import { Canvas } from './canvas/canvas';
 import { Home } from './home/home';
 import { Dashboard } from './dashboard/dashboard';
 
 export default function App() {
+  const [currentUser, setCurrentUser] = useState(null);
+
+  useEffect(() => {
+    const storedUser = localStorage.getItem('CoCreateUser');
+    if (storedUser) {
+      setCurrentUser(storedUser);
+    }
+  }, [])
+
+  function handleLogin(username) {
+    localStorage.setItem('CoCreateUser', username);
+    setCurrentUser(username);
+  }
+
+  function handleLogout() {
+    localStorage.removeItem('CoCreateUser');
+    setCurrentUser(null);
+  }
+
+
   return (
     <BrowserRouter>
       <div className="body">
         <Routes>
-            <Route path='/' element={<Login />} />
-            <Route path='/home' element={<Home />} />
-            <Route path='/dashboard' element={<Dashboard />} />
-            <Route path='/canvas' element={<Canvas />} />
+            <Route 
+              path='/' 
+              element={<Login onLogin={handleLogin} />}
+              exact
+            />
+            <Route 
+              path='/home' 
+              element={<Home currentUser={currentUser} onLogout={handleLogout} />}            
+            />
+            <Route 
+              path='/dashboard' 
+              element={<Dashboard currentUser={currentUser} onLogout={handleLogout} />}              
+            />
+            <Route 
+              path='/canvas' 
+              element={<Canvas currentUser={currentUser} onLogout={handleLogout} />} 
+            />
             <Route path='*' element={<NotFound />} />
         </Routes>
       </div>
