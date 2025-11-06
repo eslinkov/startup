61d0c46a862929a73c3660ed484cebdd7c9524ab
Emma <emma.slinkov@gmail.com>
2025-11-05 20:44:42 -0700

    Added core Middleware to index.js

--- service/index.js
+++ service/index.js
@@ -1,5 +1,17 @@
+// =====Imports===== //
 const express = require('express');
+const cookieParser = require('cookie-parser');
+
 const app = express();
+
+// =====Middleware===== //
+app.use(express.json());
+
+app.use(cookieParser());
+
+app.use(express.static('public'));
+
+// =====Engine===== //
 
 const port = process.argv.length > 2 ? process.argv[2] : 4000;
 
