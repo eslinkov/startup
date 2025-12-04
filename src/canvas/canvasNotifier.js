a09253f28f06cf0166f22457e476c5080a39b7a7
Emma <emma.slinkov@gmail.com>
2025-12-03 19:52:28 -0700

    Add WebSocket connect method

--- src/canvas/canvasNotifier.js
+++ src/canvas/canvasNotifier.js
@@ -3,7 +3,27 @@ class CanvasNotifier {
         this.socket = null;
         this.observers = [];
     }
+
+    connect () {
+        const protocol = window.location.protocol === 'http:' ? 'ws' : 'wss';
+        this.socket = new WebSocket(`${protocol}://${window.location.host}/ws`);
+
+        this.socket.onopen = () => {
+            console.log('WebSocket connected');
+        };
+
+        this.socket.onmessage = (event) => {
+            const msg = JSON.parse(event.data);
+            // this.notifyObservers(msg);
+            console.log('Received:', msg);
+        };
+
+        this.socket.onclose = () => {
+            console.log('WebSocket disconnected');
+        };
+    }
 }
 
 const notifier = new CanvasNotifier();
 export default notifier;
+
